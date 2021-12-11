3e3f58d5e329af5d139a96f55010c470c4fa62a5
ashcik-00 <ashik71216@gmail.com>
2021-12-11 15:15:01 -0600

    if i apply this commit, it will save the code to clean the css file and move it from src to dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,17 @@
 const { src, dest, series } = require("gulp");
+const cleanCSS = require("gulp-clean-css");
+const sourcemaps = require("gulp-sourcemaps");
+
+function minifyCss() {
+  return src("src/*.css")
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write())
+    .pipe(dest("dist"));
+}
 
 function moveFiles() {
   return src("src/*.html").pipe(dest("dist"));
 }
 
-exports.default = series(moveFiles);
+exports.default = series(moveFiles, minifyCss);
